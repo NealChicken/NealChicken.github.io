77f007ca09c54bde56e7dbc12e960a657ea888db
Trumoocheese <trumoocheese@gmail.com>
2017-11-06 14:56:29 -0600

    Update flashing.js

--- flashing.js
+++ flashing.js
@@ -4,5 +4,5 @@ function flashBodyBackground() {
 	document.getElementsByTagName("body")[0].style.backgroundColor = _nextColor;
 	if (_nextColor === "#fff42d") _nextColor = "#f909dd";
 	else _nextColor = "#fff42d";
-	setTimeout(flashBodyBackground, 300);
+	setTimeout(flashBodyBackground, 100);
 }
