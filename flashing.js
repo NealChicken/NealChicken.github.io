49569ae60a93e9bc90beec5edf08765c893bcb57
Kai Page <kaibug@gmail.com>
2017-11-13 17:03:08 -0600

    Moved some things around

--- flashing.js
+++ flashing.js
@@ -1,8 +1,9 @@
-let _nextColor = 0;
 const COLORS = ["#fff42d", "#f909dd"];
-
-let _nextImage = 0;
 const IMAGES = ["/images/lechickens.png", "/images/lechickens2.png"];
+const FLASH_DELAY = 50; // milliseconds between flashes
+
+let _nextColor = 0;
+let _nextImage = 0;
 
 function initFlashing() {
 	_loadImage(0);
@@ -16,6 +17,7 @@ function _loadImage(num) {
 		document.getElementById("img-load-area").innerHTML += "<img src=\"" + this.src + "\" />";
 		_loadImage(num+1);
 	}
+	dlImage.src = IMAGES[num];
 }
 
 function _flashBodyBackground() {
@@ -23,5 +25,5 @@ function _flashBodyBackground() {
 	document.getElementsByTagName("body")[0].style.backgroundImage = "url('" + IMAGES[_nextImage] + "')";
 	_nextColor++; _nextColor %= COLORS.length;
 	_nextImage++; _nextImage %= IMAGES.length;
-	setTimeout(_flashBodyBackground, 50);
+	setTimeout(_flashBodyBackground, FLASH_DELAY);
 }
